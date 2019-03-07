0d7e0714e257b945c8782191e530d7dba07ef3fd
Micaela Jawor <micaelajawor@yahoo.com>
2019-03-06 16:47:24 -0800

    fix errors

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -8,45 +8,46 @@ namespace HairSalon.Controllers
   public class StylistsController : Controller
   {
 
-    // [HttpGet("/stylists")]
-    // public ActionResult Index()
-    // {
-    //   List<Stylist> allStylists = Stylist.GetAll();
-    //   return View(allStylists);
-    // }
-    //
-    // [HttpGet("/stylists/new")]
-    // public ActionResult New()
-    // {
-    //   return View();
-    // }
-    //
-    // [HttpPost("/stylists")]
-    // public ActionResult Create(string stylistDetails)
-    // {
-    //   Stylist newStylist = new Stylist(stylistDetails);
-    //   List<Stylist> allStylists = Stylist.GetAll();
-    //   return View("Index", allStylists);
-    // }
-    //
-    // [HttpGet("/stylists/{id}")]
-    // public ActionResult Show(int id)
-    // {
-    //   Dictionary<string, object> model = new Dictionary<string, object>();
-    //   Stylist selectedStylist = Stylist.Find(id);
-    //   List<Client> stylistClients = selectedStylist.GetClients();
-    //   model.Add("stylist", selectedStylist);
-    //   model.Add("clients", stylistClients);
-    //   return View(model);
-    // }
-    //
+    [HttpGet("/stylists")]
+    public ActionResult Index()
+    {
+      List<Stylist> allStylists = Stylist.GetAll();
+      return View(allStylists);
+    }
+
+    [HttpGet("/stylists/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/stylists")]
+    public ActionResult Create(string stylistDetails)
+    {
+      Stylist newStylist = new Stylist(stylistDetails);
+      newStylist.Save();
+      List<Stylist> allStylists = Stylist.GetAll();
+      return View("Index", allStylists);
+    }
+
+    [HttpGet("/stylists/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Stylist selectedStylist = Stylist.Find(id);
+      List<Client> stylistClients = selectedStylist.GetClients();
+      model.Add("stylist", selectedStylist);
+      model.Add("clients", stylistClients);
+      return View(model);
+    }
+
     // // This one creates new Clients within a given Stylist, not new Stylists:
     // [HttpPost("/stylists/{stylistId}/clients")]
     // public ActionResult Create(int stylistId, string clientName)
     // {
     //   Dictionary<string, object> model = new Dictionary<string, object>();
     //   Stylist foundStylist = Stylist.Find(stylistId);
-    //   Client newClient = new Client(clientName);
+    //   Client newClient = new Client(clientName, stylistId);
     //   newClient.Save();
     //   foundStylist.AddClient(newClient);
     //   List<Client> stylistClients = foundStylist.GetClients();
