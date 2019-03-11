225eeb008bc1b12b4281fef5011f0ecf762b06fc
Micaela Jawor <micaelajawor@yahoo.com>
2019-03-11 12:56:19 -0700

    delete httppost stylists/{stylistId}/clients controller

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -40,21 +40,7 @@ namespace HairSalon.Controllers
     //   model.Add("clients", stylistClients);
     //   return View(model);
     // }
-    //
-    // // This one creates new Clients within a given Stylist, not new Stylists:
-    // [HttpPost("/stylists/{stylistId}/clients")]
-    // public ActionResult Create(int stylistId, string clientName)
-    // {
-    //   Dictionary<string, object> model = new Dictionary<string, object>();
-    //   Stylist foundStylist = Stylist.Find(stylistId);
-    //   Client newClient = new Client(clientName, stylistId);
-    //   newClient.Save();
-    //   // foundStylist.AddClient(newClient);
-    //   List<Client> stylistClients = foundStylist.GetClients();
-    //   model.Add("clients", stylistClients);
-    //   model.Add("stylist", foundStylist);
-    //   return View("Show", model);
-    // }
+
 
   }
 }
